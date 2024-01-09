2fba8eb6a779922263bfbd8a6e54351a2f6cef37
aspsptyd <devadiroot@gmail.com>
2024-01-09 11:32:23 +0700

    CI: Set up deployment workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,18 +8,19 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://aspsptyd.github.io',
+  trailingSlash: false,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/journal-sarangkamarang/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'aspsptyd', // Usually your GitHub org/user name.
+  projectName: 'journal-sarangkamarang', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
