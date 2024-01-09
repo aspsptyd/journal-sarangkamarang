af3ac7605dabdeb3c3909ce41404e201b49680db
aspsptyd <devadiroot@gmail.com>
2024-01-09 11:51:59 +0700

    Update again about link access

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/aspsptyd/journal-sarangkamarang/tree/master/docs/',
+            'https://github.com/aspsptyd/journal-sarangkamarang/tree/master/',
         },
         blog: {
           showReadingTime: true,
